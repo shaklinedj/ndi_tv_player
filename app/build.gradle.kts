586686cf83d6d8c8d0ea8e5a52b36111b6af6ab8
shaklinedj <hernan.laurel@gmail.com>
2026-03-21 00:24:03 -0300

    Bump version to 1.6-perf for Firebase Distribution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.dreamscasino.nditv"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.5"
+        versionCode = 7
+        versionName = "1.6-perf"
 
         externalNativeBuild {
             cmake {
